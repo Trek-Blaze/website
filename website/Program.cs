3192aaa3f4ffd88ce2f5eb3472b9a28017bfe34e
Nikhil Sarwara <tymnec@gmail.com>
2023-11-22 16:51:46 +0530

    require setup done! - for ui components :tada:

--- website/Program.cs
+++ website/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+using System.ComponentModel;
 
 namespace website
 {
@@ -10,6 +12,9 @@ namespace website
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            // Add Radzen Component Services
+            builder.Services.AddRadzenComponents();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
